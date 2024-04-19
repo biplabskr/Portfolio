e34b1dde7a6f6599cf3c58cc082ad2631b79b018
Biplab Sarkar <biplabskr24@gmail.com>
2024-04-19 23:58:31 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Nav from "./components/Nav"
+// import Banner from "./components/Banner"
+// import Service from "./components/Service"
+// import Work from "./components/Work"
+// import Home from "./components/Home"
+// import About from "./components/About"
+import Background from "./components/Background"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+
+    
+
+      
+      <Background/>
+      
+     
+      
+      
     </div>
+    
   );
 }
 
